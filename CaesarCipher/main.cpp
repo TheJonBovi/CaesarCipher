53cd7d8c9a2d3ad51ca130a0df04bdb9bf947b8b
TheJonBovi <deraitaa@gmail.com>
2018-01-27 09:20:17 -0800

    initial implementation of menu
    Still need to modify and adjust switch statement
    New gitignore, maybe will work?

--- CaesarCipher/main.cpp
+++ CaesarCipher/main.cpp
@@ -35,8 +35,41 @@ int main()
 		if (isalpha(c)) c = tolower(c);
 		plaintext[currentChar] = c;
 	}
-	//Some Test comment
-	cout << plaintext;
 
+	// Code assistance from http://www.cplusplus.com/forum/beginner/21033/
+	int choice;
+	bool programRun = true;
+	while (programRun != false) 
+	{
+		cout << "**********CaesarCipher*********\n";
+		cout << " 1 - Encrypt\n";
+		cout << " 2 - Decrypt\n";
+		cout << " 3 - Exit.\n";
+		cout << " Enter your choice and press return: ";
+
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+			cout << "Encypting plaintext...\n";
+			// rest of code here
+			break;
+		case 2:
+			cout << "Decrypting ciphertext...\n";
+			// rest of code here
+			break;
+		case 3:
+			cout << "End of Program.\n";
+			programRun = false;
+			break;
+		default:
+			cout << "Not a Valid Choice. \n";
+			cout << "Choose again.\n";
+			cin >> choice;
+			break;
+		}
+
+	}
 	return 0;
 }
