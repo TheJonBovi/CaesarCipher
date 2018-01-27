3c48d3341140de621c0bae348b1a00e21aaa9ee2
TheJonBovi <35358211+TheJonBovi@users.noreply.github.com>
2018-01-27 13:18:16 -0800

    updated menu
    Having issue with string to char* for the algorithm.

--- CaesarCipher/main.cpp
+++ CaesarCipher/main.cpp
@@ -10,7 +10,8 @@ using namespace std;
 
 int main()
 {	//string filename;
-	string plaintext = "";
+	string plaintext = nullptr;
+
 	ifstream* fin = nullptr;
 	try
 	{	fin = new ifstream("plaintextfile.txt");
@@ -24,12 +25,11 @@ int main()
 		cout<< error.what();
 	}
 
-	
-	cout<< plaintext;
+	//cout << plaintext;
 	
 	//Routines to make sure plaintext is all lowercase
 
-	for (size_t currentChar; currentChar > plaintext.size(); currentChar++)
+	for (size_t currentChar{}; currentChar > sizeof(plaintext); currentChar++)
 	{
 		char c = plaintext[currentChar];
 		if (isalpha(c)) c = tolower(c);
@@ -38,8 +38,8 @@ int main()
 
 	// Code assistance from http://www.cplusplus.com/forum/beginner/21033/
 	int choice;
-	bool programRun = true;
-	while (programRun != false) 
+	auto programRun = true;
+	while (programRun) 
 	{
 		cout << "**********CaesarCipher*********\n";
 		cout << " 1 - Encrypt\n";
@@ -49,25 +49,56 @@ int main()
 
 		cin >> choice;
 
+
 		switch (choice)
 		{
 		case 1:
-			cout << "Encypting plaintext...\n";
-			// rest of code here
+		{
+			int key{};
+
+			cout << "Enter Key: \n";
+			cin >> key;
+
+			char* ciphertext = nullptr;
+
+			Caesar_En(plaintext, ciphertext, key);
+			fstream cipherStream;
+			cipherStream.open("ciphertextfile.txt");
+			cipherStream << ciphertext;
+			cipherStream.close();
+
 			break;
+			}
 		case 2:
-			cout << "Decrypting ciphertext...\n";
-			// rest of code here
+		{
+			int key{};
+
+			cout << "Enter Key: \n";
+			cin >> key;
+
+			char* ciphertext = nullptr;
+
+			Caesar_De(ciphertext, plaintext, key);
+			fstream plainStream;
+			plainStream.open("ciphertextfile.txt");
+			plainStream << ciphertext;
+			plainStream.close();
+
 			break;
+		}
 		case 3:
+		{
 			cout << "End of Program.\n";
 			programRun = false;
 			break;
+		}
 		default:
+		{
 			cout << "Not a Valid Choice. \n";
 			cout << "Choose again.\n";
 			cin >> choice;
 			break;
+		}
 		}
 
 	}
